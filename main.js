68be7da5d5b01660dfed64cabbde6a9db3c76380
giuseppedelcorso <Giuseppedecoboolean@gmail.com>
2023-11-07 16:23:06 +0100

    sv

--- main.js
+++ main.js
@@ -1,42 +1,38 @@
 
 let btn = document.getElementById("bottone")
+btn.addEventListener("click", function(){
+    grigliaGen()
+    
+})
 
-
+    
 function grigliaGen(){
     
-    btn.addEventListener("click", function(){
-        
-        grigliaGen()
-        
-        let numeroCelle = 100
-        let grid = document.getElementById("griglia")
-        
-        for (let i = 1; i <= numeroCelle; i++) {
-            let nCelle = i + 0;
-            const cella = document.createElement("div");
-            cella.classList.add("square");
-            cella.classList.add("spazio")
-            grid.appendChild(cella);
-            cella.innerHTML = nCelle
+    let numeroCelle = 100
+    let grid = document.getElementById("griglia")
     
-            
+    for (let i = 1; i <= numeroCelle; i++) {
+        let nCelle = i + 0;
+        const cella = document.createElement("div");
+        cella.classList.add("square");
+        cella.classList.add("spazio")
+        grid.appendChild(cella);
+        cella.innerHTML = nCelle
+
+        
+
+        cella.addEventListener("click", function(){
     
-            cella.addEventListener("click", function(){
-        
-            console.log("cliccata", nCelle)
-        
-            cella.classList.toggle("colorata")
-        
-            
+        console.log("cliccata", nCelle)
     
+        cella.classList.toggle("colorata")
     
-            });
         
-        }
-    })
 
 
-
+        });
+    
+    }
 
 }    
 
